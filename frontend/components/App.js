38503a1290a18658ee1a91651782082f9a58726f
renee.troiani@gmail.com <renee.troiani@gmail.com>
2022-04-12 14:16:58 -0600

    state intialized, axios call after mounting

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -1,9 +1,40 @@
 import React from 'react'
+import axios from 'axios'
 
 const URL = 'http://localhost:9000/api/todos'
 
+
 export default class App extends React.Component {
+
+  state = {
+    todos: [],
+    message: ''
+  }
+  componentDidMount() {
+    axios.get(URL)
+      .then(res => {
+        console.log(res.data)
+        this.setState({
+          todos: res.data.data,
+          message: res.data.message
+        })
+      })
+      .catch(err => {
+        console.log(err)
+        this.setState({
+          message: `Oops! We couldn't find your todo list 🥲`
+        })
+      })
+  }
   render() {
-    return null
+    return (
+      <div className='App'>
+        <h2>{this.state.message}</h2>
+        {this.state.todos.map(todo => {
+          const { id, name, completed } = todo
+          return <p key={id}>{name} {completed ? '✔️' : ''}</p>
+        })}
+      </div>
+    )
   }
 }
